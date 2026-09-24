0398b2b7ec728052963d0d67e06dafc5a4d80ceb
saitejballav-prog <saitejballav@gmail.com>
2026-09-24 19:39:14 +0530

    Update index.js for new user test user 5

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ const claims = [
     receivedDate: "2026-09-22"
   },
   {
-    dcn: "DCN10003",
-    memberId: "TEST003",
+    dcn: "DCN10005",
+    memberId: "TEST005",
     status: "RECEIVED",
     receivedDate: "2026-09-23"
   }
